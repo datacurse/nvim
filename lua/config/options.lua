6fa751e91e35d2fe9f18b3da95b8bf5ca5c767df
loki <fiji8800@gmail.com>
2025-01-31 15:21:45 +0300

    added clipboard support for rpi

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,6 +1,17 @@
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 vim.g.have_nerd_font = true
+vim.g.clipboard = {
+  name = 'OSC 52',
+  copy = {
+    ['+'] = require('vim.ui.clipboard.osc52').copy '+',
+    ['*'] = require('vim.ui.clipboard.osc52').copy '*',
+  },
+  paste = {
+    ['+'] = require('vim.ui.clipboard.osc52').paste '+',
+    ['*'] = require('vim.ui.clipboard.osc52').paste '*',
+  },
+}
 vim.opt.number = true
 vim.opt.mouse = 'a'
 vim.opt.textwidth = 80
