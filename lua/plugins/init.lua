f5259b06d95390fdcab0977dec77e34ce010711c
loki <fiji@gmail.com>
2025-03-23 19:16:34 +0300

    ...

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,1 +1,14 @@
-return {}
+return {
+  {
+    'LazyVim/LazyVim',
+    opts = function(_, opts)
+      -- add keymaps
+      opts.keys = opts.keys or {}
+      vim.list_extend(opts.keys, {
+        { '<C-a>', 'ggVG', mode = 'n', desc = 'Select all text' },
+        { '<C-a>', '<Esc>ggVG', mode = 'v', desc = 'Select all text' },
+        { '<C-a>', '<Esc>ggVG', mode = 'i', desc = 'Select all text' },
+      })
+    end,
+  },
+}
