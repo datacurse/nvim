d920588167ecc88f972a5d9919d5422d0c40f975
loki <fiji@gmail.com>
2025-03-23 19:12:26 +0300

    ...

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,3 @@
+vim.keymap.set('n', '<C-a>', 'ggVG', { desc = 'Select all text' })
+vim.keymap.set('v', '<C-a>', '<Esc>ggVG', { desc = 'Select all text' })
+vim.keymap.set('i', '<C-a>', '<Esc>ggVG', { desc = 'Select all text' })
