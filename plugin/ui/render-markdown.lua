1aca6a5ca17566cb6bf280f2395c2f72c7faa307
loki <fiji@gmail.com>
2025-03-31 03:02:25 +0300

    13

--- plugin/ui/render-markdown.lua
+++ plugin/ui/render-markdown.lua
@@ -5,29 +5,20 @@ end
 local render_markdown = require('render-markdown')
 
 render_markdown.setup({
-  only_render_image_at_cursor = true,
   file_types = { 'markdown' },
+  only_render_image_at_cursor = true,
   completions = { blink = { enabled = true } },
-
   heading = {
     render_modes = true,
     icons = { '󰬺  ', '󰬻  ', '󰬼  ', '󰬽  ', '󰬾  ', '󰬿  ' },
     position = 'inline',
-    -- signs = { '󰫎 ' },
-    sign = false,
     backgrounds = {},
   },
-
-  code = {
-    sign = false,
-  },
-
+  code = {},
   dash = {},
-
   bullet = {
     icons = { '●', '○', '◆', '◇' },
   },
-
   checkbox = {
     unchecked = {
       icon = '󰄱 ',
@@ -41,68 +32,30 @@ render_markdown.setup({
       todo = { raw = '[-]', rendered = ' ', highlight = 'RenderMarkdownTodo' },
     },
   },
-
   quote = {
     repeat_linebreak = true,
   },
-
   pipe_table = {
     preset = 'round',
     alignment_indicator = '',
   },
-
   callout = {},
-
   link = {
     custom = {
       python = {
         pattern = '%.py$',
         icon = '󰌠 ',
       },
+      markdown = {
+        pattern = '%.md$',
+        icon = '󰍔 ',
+      },
     },
   },
-
-  --
-  -- -- -- LaTeX support configuration
-  -- -- latex = {
-  -- --   enabled = true,
-  -- --   conceal = { enabled = true },
-  -- --   renderer = "pylatexenc",
-  -- -- },
-  --
-
-  --
-  -- -- Org-mode like indent guide
-  -- indent = {
-  --   enabled = true,
-  --   icon = '│', -- Vertical bar for indent guide
-  --   color = { enabled = true },
-  -- },
-  --
-  -- -- Completions
-  -- completions = {
-  --   lsp = { enabled = true }, -- In-process LSP completions
-  --   blink = { enabled = true }, -- Blink.cmp integration
-  -- },
-  --
-  -- -- Window options when in rendered mode
-  -- window_options = {
-  --   conceallevel = 3, -- Hide characters for concealed elements
-  --   wrap = true, -- Enable line wrapping for better readability
-  --   linebreak = true, -- Break lines at word boundaries
-  -- },
+  sign = { enabled = false },
+  indent = { enabled = false },
 })
 
--- Set up keymaps for toggling rendering
 vim.keymap.set('n', '<leader>mr', function()
   render_markdown.toggle()
 end, { desc = '[M]arkdown [R]ender toggle' })
-
--- Optional additional keymaps for specific functions
-vim.keymap.set('n', '<leader>me', function()
-  render_markdown.expand()
-end, { desc = '[M]arkdown [E]xpand anti-conceal margin' })
-
-vim.keymap.set('n', '<leader>mc', function()
-  render_markdown.contract()
-end, { desc = '[M]arkdown [C]ontract anti-conceal margin' })
